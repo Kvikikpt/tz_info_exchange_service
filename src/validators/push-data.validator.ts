b2f861d316f42825422be5ca2f9a084d793e36db
YAZIKOV Stepan <stepan.yazikov@raiffeisen.ru>
2022-03-02 21:30:22 +0700

    fix route

--- src/validators/push-data.validator.ts
+++ src/validators/push-data.validator.ts
@@ -2,11 +2,11 @@ import * as yup from 'yup';
 import { DateTime } from 'luxon';
 
 export const pushDataValidator = yup.object({
-  data: yup
+  data: yup.string().required().trim(),
+  accessTimesCount: yup.number().required(),
+  expirationTime: yup
     .string()
     .required()
     .trim()
     .test((value) => !!value && DateTime.fromISO(value).isValid),
-  accessTimesCount: yup.number().required(),
-  expirationTime: yup.date().required(),
 });
